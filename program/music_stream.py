b11c00880b063aea845e87e4cfc9ca7c4e3c5c00
SEMOELKBER21 <93216241+SEMOELKBER21@users.noreply.github.com>
2022-03-05 03:25:07 +0200

    Update music_stream.py

--- program/music_stream.py
+++ program/music_stream.py
@@ -162,7 +162,7 @@ async def play_tg_file(c: Client, m: Message, replied: Message = None, link: str
         )
 
 
-@Client.on_message(command(["/play", f"play@{BOT_USERNAME}", "شغل", "تشغيل"]) & other_filters)
+@Client.on_message(command(["/play", "play", f"play@{BOT_USERNAME}", "شغل", "تشغيل"]) & other_filters)
 @check_blacklist()
 @require_admin(permissions=["can_manage_voice_chats", "can_delete_messages", "can_invite_users"], self=True)
 async def play(c: Client, m: Message):
